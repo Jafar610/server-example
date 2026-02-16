c7d34bb4ee26abfff985766fdf0570ed89763f04
Jafar610 <jafarmohamed955@gmail.com>
2026-02-16 23:48:13 +0300

    file updated

--- apple-express.js
+++ apple-express.js
@@ -1,11 +1,28 @@
+// const express = require('express');
+
+// const server = express();
+// server.use(express.static('./Apple'));
+
+// server.listen(4040, (err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("Server is running on port 4040");
+// })
+
+
+
 const express = require('express');
+
 
 const server = express();
 server.use(express.static('./Apple'));
 
-server.listen(4040, (err)=>{
+const port = 5043;
+server.listen(port, (err)=>{
     if(err){
-        console.log(err);
+        console.error(err);
     }
-    console.log("Server is running on port 4040");
+
+    console.log('Your server is running on: http://localhost:' + port);
 })
