6be6d2b6bfc32404b9731e07258060a51ef5dbde
Jafar610 <jafarmohamed955@gmail.com>
2026-02-16 23:58:33 +0300

    file updated

--- apple-server.js
+++ apple-server.js
@@ -31,37 +31,74 @@
 // });
 
 
+// const http = require("http");
+// const fs = require("fs");
+// const mimeType = require("mime-types");
+
+// const server = http.createServer((req, res)=>{
+//  let reqPath = req.url;
+//  if(reqPath ==='/'){
+//    reqPath = '/index.html';
+//  }
+
+//  let urlPath = "Apple" + reqPath;
+//  console.log(urlPath);
+
+//   const type = mimeType.lookup(reqPath);
+
+//   fs.readFile(`${urlPath}`, (err, data)=>{
+//      if(err){
+//       res.write('<h1>Page Not Found</h1>');
+//       res.end();
+//      }
+//      else{
+//         res.writeHead(200, {'content-type': type});
+//         res.write(data);
+//         res.end();
+//      }
+//   })
+// });
+
+// server.listen(4412, (err)=>{
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log("Your server is running.");
+// })
+
 const http = require("http");
 const fs = require("fs");
 const mimeType = require("mime-types");
 
+
+
 const server = http.createServer((req, res)=>{
- let reqPath = req.url;
- if(reqPath ==='/'){
-   reqPath = '/index.html';
- }
+  let urlPath = req.url;
+  if(urlPath === '/'){
+    urlPath = '/index.html';
+  }
 
- let urlPath = "Apple" + reqPath;
- console.log(urlPath);
+  
+  const requestPath = 'Apple' + urlPath;
+  console.log(requestPath)
+const type = mimeType.lookup(requestPath);
 
-  const type = mimeType.lookup(reqPath);
-
-  fs.readFile(`${urlPath}`, (err, data)=>{
-     if(err){
-      res.write('<h1>Page Not Found</h1>');
+  fs.readFile(requestPath, (err, data)=>{
+    if(err){
+      res.write('<h1> Page Not Found</h1>');
       res.end();
-     }
-     else{
-        res.writeHead(200, {'content-type': type});
-        res.write(data);
-        res.end();
-     }
+    }
+    else{
+      res.writeHead(200,{'content-type':type});
+      res.write(data);
+      res.end();
+    }
   })
 });
 
-server.listen(4412, (err)=>{
+server.listen(1212, (err)=>{
   if(err){
     console.log(err);
   }
-  console.log("Your server is running.");
-})
+  console.log('Your server is running');
+} )
