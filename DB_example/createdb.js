603085bfa496fb5faf88c94324c5336fe18b8fdb
Jafar610 <jafarmohamed955@gmail.com>
2026-02-19 16:52:05 +0300

    file update

--- DB_example/createdb.js
+++ DB_example/createdb.js
@@ -1,64 +1,79 @@
+// const express = require('express');
+// const mysql = require('mysql2');
+
+// const server = express();
+
+// server.listen(3000, (err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('Server is running on port 3000');
+// });
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'testdb'
+// });
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('Connected to MySQL database');
+// });
+
+
+// server.get('/createdb', (req, res)=>{
+//     const customer = `CREATE TABLE if not exists customers(
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         name VARCHAR(255) NOT NULL,
+//         email VARCHAR(255) NOT NULL
+//     )`;
+
+//     const addres = `CREATE TABLE if not exists address(
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         customer_id INT NOT NULL,
+//         address VARCHAR(255) NOT NULL,
+//         FOREIGN KEY (customer_id) REFERENCES customers(id)
+//     )`;
+
+//     const company = `CREATE TABLE if not exists company(
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         name VARCHAR(255) NOT NULL,
+//         address VARCHAR(255) NOT NULL,
+//         customer_id INT NOT NULL,
+//         FOREIGN KEY (customer_id) REFERENCES customers(id)
+//     )`;
+
+//     connection.query(customer, (err, results, fields)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//     });
+//     connection.query(addres, (err, results, fields)=>{
+//         if(err){
+//             console.log(err);  
+//         }
+//     });
+//     connection.query(company, (err, results, fields)=>{
+//         if(err){
+//             console.log(err);  
+//         }   
+//     });
+//     res.end('Database and tables created successfully');
+// })
+
+
 const express = require('express');
 const mysql = require('mysql2');
 
+
 const server = express();
 
-server.listen(3000, (err)=>{
+
+server.listen(3022,(err)=>{
     if(err){
         console.log(err);
     }
-    console.log('Server is running on port 3000');
-});
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'testdb'
-});
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log('Connected to MySQL database');
-});
-
-
-server.get('/createdb', (req, res)=>{
-    const customer = `CREATE TABLE if not exists customers(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL
-    )`;
-
-    const addres = `CREATE TABLE if not exists address(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        customer_id INT NOT NULL,
-        address VARCHAR(255) NOT NULL,
-        FOREIGN KEY (customer_id) REFERENCES customers(id)
-    )`;
-
-    const company = `CREATE TABLE if not exists company(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        address VARCHAR(255) NOT NULL,
-        customer_id INT NOT NULL,
-        FOREIGN KEY (customer_id) REFERENCES customers(id)
-    )`;
-
-    connection.query(customer, (err, results, fields)=>{
-        if(err){
-            console.log(err);
-        }
-    });
-    connection.query(addres, (err, results, fields)=>{
-        if(err){
-            console.log(err);  
-        }
-    });
-    connection.query(company, (err, results, fields)=>{
-        if(err){
-            console.log(err);  
-        }   
-    });
-    res.end('Database and tables created successfully');
+    console.log('Your server is running on 3022');
 })
